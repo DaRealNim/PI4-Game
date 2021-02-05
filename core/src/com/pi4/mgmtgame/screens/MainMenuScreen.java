35a1f81a4c9093065ff6aecc6bad40f097229822
krkn <claudio.t1337@gmail.com>
2021-02-05 20:15:44 +0100

    Removed old assets, now using scene2d library with .atlas and .json files. Main Menu now works.

--- core/src/com/pi4/mgmtgame/screens/MainMenuScreen.java
+++ core/src/com/pi4/mgmtgame/screens/MainMenuScreen.java
@@ -3,40 +3,87 @@ package com.pi4.mgmtgame.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.pi4.mgmtgame.ManagementGame;
 
 public class MainMenuScreen implements Screen {
 
-	private static final int BUTTON_WIDTH = 600 / 3;
-	private static final int BUTTON_HEIGHT = 200 / 3;
-	private static final int BUTTON_XPOS = Gdx.graphics.getWidth() / 2 - BUTTON_WIDTH / 2;
-
+	private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
 	ManagementGame game;
-	
-	Texture quitButtonActive;
-	Texture quitButtonInactive;
-	Texture newGameButtonActive;
-	Texture newGameButtonInactive;
-	Texture loadButtonActive;
-	Texture loadButtonInactive;
-	
 	
 	public MainMenuScreen(ManagementGame game) {
 		this.game = game;
+		this.batch = game.batch;
 		
-		quitButtonActive = new Texture("coloredButtons/Quit_col_Button.png");
-		quitButtonInactive = new Texture("buttons/Quit_Button.png");
-		newGameButtonActive = new Texture("coloredButtons/New_Game_col_Button.png");
-		newGameButtonInactive = new Texture("buttons/New_Game_Button.png");
-		loadButtonActive = new Texture("coloredButtons/Load_col_Button.png");
-		loadButtonInactive = new Texture("buttons/Load_Button.png");
+	    camera = new OrthographicCamera();
+	    
+	    viewport = new FitViewport(ManagementGame.WIDTH, ManagementGame.HEIGHT, camera);
+        viewport.apply();
+
+	    camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+	    camera.update();
+
+	    stage = new Stage(viewport, batch);	
 	}
 	
 	@Override
 	public void show() {
-
+		Gdx.input.setInputProcessor(stage);
 		
+		Table mainTable = new Table();
+	    mainTable.setFillParent(true);
+	    mainTable.top();
+	    
+		TextureAtlas buttonAtlas = new TextureAtlas("menuButtons/ButtonStyles.atlas");
+		Skin buttonSkins = new Skin(Gdx.files.internal("menuButtons/ButtonStyles.json"), buttonAtlas);
+		
+		Button quitButton = new Button(buttonSkins, "quit");
+		Button newGameButton = new Button(buttonSkins, "default");
+		Button loadButton = new Button(buttonSkins, "load");
+		
+		quitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+		
+		newGameButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+            	game.setScreen(new MainGameScreen(game));
+            }
+        });
+		
+		
+		 
+		 mainTable.add(newGameButton).size(300, 100).padBottom(50);
+	     
+		 mainTable.row();
+	    
+	     mainTable.add(loadButton).size(300, 100);
+	    
+	     mainTable.row();
+	     
+	     mainTable.add(quitButton).size(300, 100).padTop(50);;
+	     
+	     mainTable.align(Align.bottom);
+	     
+	     stage.addActor(mainTable);
 	}
 
 	@Override
@@ -45,39 +92,15 @@ public class MainMenuScreen implements Screen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		game.batch.begin();
-		
-		if (Gdx.input.getX() < BUTTON_XPOS + BUTTON_WIDTH &&
-			Gdx.input.getX() > BUTTON_XPOS && 
-			Gdx.graphics.getHeight() - Gdx.input.getY() < 50 + BUTTON_HEIGHT && 
-			Gdx.graphics.getHeight() - Gdx.input.getY() > 0) 
-		{
-			game.batch.draw(quitButtonActive, BUTTON_XPOS, 0, BUTTON_WIDTH, BUTTON_HEIGHT);
-		}
-		else {
-			game.batch.draw(quitButtonInactive, BUTTON_XPOS, 0, BUTTON_WIDTH, BUTTON_HEIGHT);
-		}
-		
-		/*
-		if (Gdx.input.getX() < 100)
-			game.batch.draw(newGameButtonActive, BUTTON_XPOS, 200, BUTTON_WIDTH, BUTTON_HEIGHT);
-		else {
-			game.batch.draw(newGameButtonInactive, BUTTON_XPOS, 200, BUTTON_WIDTH, BUTTON_HEIGHT);
-		}
-			
-		if (Gdx.input.getX() < 100)
-			game.batch.draw(loadButtonActive, BUTTON_XPOS, 100, BUTTON_WIDTH, BUTTON_HEIGHT);
-		else {
-			game.batch.draw(loadButtonInactive, BUTTON_XPOS, 100, BUTTON_WIDTH, BUTTON_HEIGHT);
-		}*/
-		
-		game.batch.end();
+		stage.act();
+		stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-	
-		
+		viewport.update(width, height);
+		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+		camera.update();
 	}
 
 	@Override
@@ -90,7 +113,9 @@ public class MainMenuScreen implements Screen {
 	public void hide() {}
 
 	@Override
-	public void dispose() {}
+	public void dispose() {
+		stage.dispose();
+	}
 	
 
 }
