6b311692c356a0ec9bb9aea24a5b8ae251719331
DaRealNim <darealnim@gmail.com>
2021-04-09 14:06:45 +0200

    addViewController to build stuff is now in environment and not in plain

--- core/src/com/pi4/mgmtgame/blocks/Plain.java
+++ core/src/com/pi4/mgmtgame/blocks/Plain.java
@@ -22,53 +22,7 @@ public class Plain extends Environment {
 
     @Override
     public void addViewController(final AssetManager manager, final ServerInteraction server) {
-        this.manager = manager;
-        Button button = new Button(manager.get("blocks/Blocks.json", Skin.class), "plain");
-        button.setX(getGridX() * 16);
-		button.setY(getGridY() * 16);
-        button.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            	System.out.println("Clicked block at ("+getGridX()+", "+getGridY()+")");
-                Button buttonField = new Button(manager.get("popupIcons/popup.json", Skin.class), "hoe_icon");
-                Button buttonTree = new Button(manager.get("popupIcons/popup.json", Skin.class), "hoe_icon");
-                final Field f = new Field(getGridX(), getGridY());
-                final TreeField g = new TreeField(getGridX(), getGridY());
-                f.setOwnerID(server.getCurrentPlayer());
-                g.setOwnerID(server.getCurrentPlayer());
-                final Popup p = new Popup((getGridX() - 2) * 16 + 8, (getGridY() + 1) * 16, manager,buttonField, buttonTree);
-                if (server.canBuildStructure(getGridX(), getGridY(), g)) {
-                    buttonTree.addListener(new ClickListener(){
-                        @Override
-                        public void clicked(InputEvent event, float x, float y) {
-                            Map map = (Map)getParent();
-                            boolean res = server.requestBuildStructure(getGridX(), getGridY(), g);
-                            System.out.println("Could build TreeF at "+getGridX()+", "+getGridY()+": "+res+"id :");
-                            updateMap(manager, server);
-                            p.remove();
-                        }
-                    });
-                } else {
-                    buttonTree.getColor().a = (float)0.3;
-                }
-                if (server.canBuildStructure(getGridX(), getGridY(), f)) {
-                    buttonField.addListener(new ClickListener(){
-                        @Override
-                        public void clicked(InputEvent event, float x, float y) {
-                            Map map = (Map)getParent();
-                            boolean res = server.requestBuildStructure(getGridX(), getGridY(), f);
-                            System.out.println("Could build structure at "+getGridX()+", "+getGridY()+": "+res+"id :");
-                            updateMap(manager, server);
-                            p.remove();
-                        }
-                    });
-                } else {
-                    buttonField.getColor().a = (float)0.3;
-                }
-                getStage().addActor(p);
-            }
-        });
-        setButton(button);
+        super.addViewController(manager, server, "plain");
     }
 
     @Override
